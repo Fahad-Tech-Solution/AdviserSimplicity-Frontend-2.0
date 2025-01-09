b6613330cd7f6ce1707b68cd57a4133adf1b7d50
WahabHameed2160 <wahabhameed2160@gamil.com>
2025-01-09 17:36:21 +0500

    Completed Platform Investments and Investment Bond of Financial Investments)

--- cypress/e2e/Financial Investments/Cards.cy.js
+++ cypress/e2e/Financial Investments/Cards.cy.js
@@ -321,6 +321,225 @@ class FinancialInvestments {
       .click();
 
       cy.get('#clientaustralianShareMarket').should("not.have.value", "");
+
+     //Platform Investments
+     cy.get(':nth-child(4) > .py-4').within(() => {
+      cy.contains("Platform Investments");
+      cy.get("img");
+      cy.contains("Admin");
+      cy.get(
+        "label.mb-0.bg-secondary.rounded-circle.text-light.py-1.px-2.curser-pointer"
+      ).click();
+    });
+
+    cy.get(".modal-content").within(() => {
+      cy.contains("Platform Investments");
+      cy.get(".btn-close");
+      cy.get(".table").within(() => {
+        cy.contains("Owner");
+        cy.get('[style="width: 50%;"]').contains("Admin");
+        cy.contains("Current Balance");
+      });
+    });
+
+    cy.get("#button-addon2").click();
+
+    cy.get('[style="display: block;"] > .modal-dialog > .modal-content').within(
+      () => {
+        cy.contains("Admin_Platform Investments Detail");
+        cy.contains("How many Platforms does Admin have :");
+        cy.get("#NumberOfMap").clear().type(1);
+      }
+    );
+    //Inner Table
+    cy.get(
+      '[style="display: block;"] > .modal-dialog > .modal-content > .modal-body > form > :nth-child(1) > .col-md-12 > .row > .mt-4'
+    ).within(() => {
+      cy.contains("No#");
+      cy.contains("1");
+
+      cy.contains("Platform Name");
+      cy.get("#platformName0").select("For Testing Purpose");
+
+      cy.contains("Account Number");
+      cy.get("#accountNumber0").clear().type("45");
+
+      cy.contains("Portfolio Cost Base");
+      cy.get("#totalPortfolioCost0").clear().type("$45");
+
+      cy.contains("Annual Advice Service Fee");
+      cy.get("#serviceFee0").clear().type("$3");
+      cy.get("#serviceFeeType0").select("Weekly");
+
+      cy.contains("Portfolio Value");
+      cy.get("#button-addon2").click();
+    });
+
+    //Portfolio Value inner card
+    cy.get(":nth-child(7) > .modal-dialog > .modal-content").within(() => {
+      cy.contains("For Testing Purpose_Portfolio Value");
+      cy.contains("How many Underlying Investments does Admin have :");
+      cy.get("#NumberOfMap").clear().type(1);
+
+      cy.contains("No#");
+      cy.contains("1");
+
+      cy.contains("Investment Option");
+    });
+
+    cy.get('.css-d07bj1 > :nth-child(1)').click();
+    
+    cy.get('#react-select-3-listbox').select('Testing(001)');  
+    cy.get('#react-select-3-option-0').click();
+
+    cy.get(":nth-child(7) > .modal-dialog > .modal-content").within(() => {
+      cy.contains("Investment Code");
+      cy.get("#investmentCode0").should("not.have.value", "");
+
+      cy.contains("Investment Value");
+      cy.get("#investmentValue0").clear().type("$22");
+
+      cy.contains("Close");
+      cy.contains("Submit").click();
+    });
+
+    // cy.get("#sharePrice0").should("not.have.value", "");
+
+    cy.get("#portfolioValue0").should("not.have.value", "");
+
+    cy.get(".row.mt-2");
+
+    cy.get('#portfolioValue0').should("not.have.value", "");
+
+   
+  
+
+    cy.get(".modal-footer")
+      .filter(":visible")
+      .within(() => {
+        cy.contains("Close");
+        cy.contains("Submit").click();
+      });
+
+       cy.contains("Cost Base");
+    cy.get("#clientCostBaseTemp").should("not.have.value", "");
+
+    cy.get(".modal-footer button.btn.bgColor.modalBtn")
+      .filter(":visible")
+      .click();
+
+      cy.get('#clientmanagedFund').should("not.have.value", "");
+
+    // End Platform Investments
+
+        //Investment Bond
+        cy.get(':nth-child(5) > .py-4').within(() => {
+          cy.contains("Investment Bond");
+          cy.get("img");
+          cy.contains("Admin");
+          cy.get(
+            "label.mb-0.bg-secondary.rounded-circle.text-light.py-1.px-2.curser-pointer"
+          ).click();
+        });
+    
+        cy.get(".modal-content").within(() => {
+          cy.contains("Investment Bond");
+          cy.get(".btn-close");
+          cy.get(".table").within(() => {
+            cy.contains("Owner");
+            cy.get('[style="width: 50%;"]').contains("Admin");
+            cy.contains("Current Balance");
+          });
+        });
+    
+        cy.get("#button-addon2").click();
+    
+        cy.get('[style="display: block;"] > .modal-dialog > .modal-content').within(
+          () => {
+            cy.contains("Admin_Investment Bond Detail");
+            cy.contains("How many Platforms does Admin have :");
+            cy.get("#NumberOfMap").clear().type(1);
+          }
+        );
+        //Inner Table
+        cy.get(
+          '[style="display: block;"] > .modal-dialog > .modal-content > .modal-body > form > :nth-child(1) > .col-md-12 > .row > .mt-4'
+        ).within(() => {
+          cy.contains("No#");
+          cy.contains("1");
+    
+          cy.contains("Platform Name");
+          cy.get('#platformName0').select('Testing Purpose', { force: true });
+
+    
+          cy.contains("Account Number");
+          cy.get("#accountNumber0").clear().type("45");
+    
+          cy.contains("Portfolio Cost Base");
+          cy.get("#totalPortfolioCost0").clear().type("$45");
+    
+          cy.contains("Annual Advice Service Fee");
+          cy.get("#serviceFee0").clear().type("$3");
+          cy.get("#serviceFeeType0").select("Weekly");
+    
+          cy.contains("Portfolio Value");
+          cy.get("#button-addon2").click();
+        });
+    
+        //Portfolio Value inner card
+        cy.get(":nth-child(7) > .modal-dialog > .modal-content").within(() => {
+          cy.contains("Testing Purpose_Portfolio Value");
+          cy.contains("How many Underlying Investments does Admin have :");
+          cy.get("#NumberOfMap").clear().type(1);
+    
+          cy.contains("No#");
+          cy.contains("1");
+    
+          cy.contains("Investment Option");
+        });
+    
+        cy.get(".css-1xc3v61-indicatorContainer").click();
+    
+        cy.get('#react-select-2-option-0').click();
+    
+        cy.get(":nth-child(7) > .modal-dialog > .modal-content").within(() => {
+          cy.contains("Investment Code");
+          cy.get("#investmentCode0").should("not.have.value", "");
+    
+          cy.contains("Investment Value");
+          cy.get("#investmentValue0").clear().type("$22");
+    
+          cy.contains("Close");
+          cy.contains("Submit").click();
+        });
+    
+
+    
+        cy.get("#portfolioValue0").should("not.have.value", "");
+    
+      
+    
+        cy.get(".modal-footer button.btn.bgColor.modalBtn")
+          .filter(":visible")
+          .click();
+    
+        cy.get("#clientCurrentBalance").should("not.have.value", "");
+        //Cost Base
+        cy.contains("Cost Base");
+        cy.get("#clientCostBaseTemp").clear().type("556");
+        cy.wait(1000);
+        cy.contains(
+          "Total must be equal to the sum of all Cost Base filled in the popup. The sum is $45"
+        );
+    
+        cy.get(".modal-footer")
+          .filter(":visible")
+          .within(() => {
+            cy.contains("Close");
+            cy.contains("Submit").click();
+          });
+    
+        // End Investment Bond
   }
 }
 
