c1b8418581888f1c0c42089163f2d02e4b296b3f
usamafaheemAhmed <usamafaheemahmed80@gmail.com>
2026-05-15 12:24:30 +0500

    browser-router

--- src/main.jsx
+++ src/main.jsx
@@ -4,33 +4,33 @@ import App from "./App.jsx";
 import "./index.css";
 import { RecoilRoot } from "recoil";
 import { ConfigProvider, notification } from "antd";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import RecoilNexus from "recoil-nexus";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ConfigProvider
-      notify={notification}
-      theme={{
-        token: {
-          /* here is your global tokens */
-          colorPrimary: "#36b446",
-        },
+    <BrowserRouter basename={"/AdviserSimplicity-Frontend-2.0/"}>
+      <ConfigProvider
+        notify={notification}
+        theme={{
+          token: {
+            /* here is your global tokens */
+            colorPrimary: "#36b446",
+          },
 
-        // components: {
-        //   Layout: {
-        //     siderBg: "#36b446",
-        //     triggerBg: "#36b446",
-        //   },
-        // },
-      }}
-    >
-      <RecoilRoot>
-        <RecoilNexus />
-        <HashRouter>
+          // components: {
+          //   Layout: {
+          //     siderBg: "#36b446",
+          //     triggerBg: "#36b446",
+          //   },
+          // },
+        }}
+      >
+        <RecoilRoot>
+          <RecoilNexus />
           <App />
-        </HashRouter>
-      </RecoilRoot>
-    </ConfigProvider>
+        </RecoilRoot>
+      </ConfigProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,12 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/AdviserSimplicity-Frontend-2.0/",
   plugins: [react()],
   server: {
     host: true, // ← allows access via IP address
     port: 5173, // optional: choose your port
   },
-  base: "/AdviserSimplicity-Frontend-2.0/",
 });
 
 //Converted Full Project into Vite Frame Work by copying and Establishing and installing Libraries
